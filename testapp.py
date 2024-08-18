a1d4c085fb1794cfaba887a2abbe067cc804844f
k-goel <kabirgoel1201@gmail.com>
2024-08-18 17:08:34 +0400

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -4,7 +4,7 @@ import requests
 # Replace with your Strava app's client ID and secret
 CLIENT_ID = '132513'
 CLIENT_SECRET = '98b0aa1146fde236a9c30822f23fe4b63acf0531'
-REDIRECT_URI = 'http://localhost:8501/'  # Update this to your deployed URL
+REDIRECT_URI = 'http://teststrava.streamlit.app/'  # Update this to your deployed URL
 AUTHORIZATION_URL = 'https://www.strava.com/oauth/authorize'
 TOKEN_URL = 'https://www.strava.com/oauth/token'
 ACTIVITIES_URL = 'https://www.strava.com/api/v3/athlete/activities'
